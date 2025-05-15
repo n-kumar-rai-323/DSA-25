05884f7169af35cda76db776280ad43bc13c10c3
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2025-05-15 09:30:06 +0545

    Java Variable

---
+++ JavaVariable.java
@@ -0,0 +1,8 @@
+public class JavaVariable {
+    public  static void main(String [] args){
+        int a = 3;
+        int b = 4;
+        int sum = a + b; 
+        System.out.println(sum);
+    }
+}
