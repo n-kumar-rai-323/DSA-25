2a0f59acbf63281a7506d25d9b5c8b3b97dfafd5
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2025-05-15 09:56:08 +0545

    Type Conversion

---
+++ TypeConversion.java
@@ -0,0 +1,8 @@
+public class TypeConversion {
+    public static void main(String [] argis){
+        int myInt = 9;
+        double myDouble = myInt;
+        System.out.println(myInt);
+        System.out.println(myDouble);// Automatic casting: int to double
+    }
+}
