bc3ced476682aad5417241d06a42d41f5d6a43c3
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2025-05-16 20:40:50 +0545

    Arithmetic Operator

---
+++ Operator/TypeOfOperators.java
@@ -0,0 +1,14 @@
+
+
+public class TypeOfOperators {
+    public static void main(String[] args) {
+        System.out.println("Arithmetic Operator");
+        int a = 10;
+        int b = 40;
+        System.out.println("Add " + (a + b));
+        System.out.println("Subtract" + (a-b));
+        System.out.println("Multiply" + ( a*b));
+        System.out.println("Division"+ (a/b));
+        System.out.println("modulo" + (a%b));
+    }
+}
