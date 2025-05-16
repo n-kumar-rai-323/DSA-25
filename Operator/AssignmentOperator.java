6e8c79ba3a41c7ebdfa0c85f4ab52326e19e1d50
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2025-05-16 21:02:56 +0545

    Assignment Operators

---
+++ Operator/AssignmentOperator.java
@@ -0,0 +1,10 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        System.out.println("Assignment Operators");
+
+        System.out.println("=");
+        System.out.println("+=");
+        System.out.println("-=");
+        System.out.println("");
+    }
+}
