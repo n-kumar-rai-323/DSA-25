8f5e1856bb08cb4f2bb527def578b0cc6678de9f
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2025-05-16 20:58:16 +0545

    Logical Operator

---
+++ Operator/LogicalOperator.java
@@ -0,0 +1,12 @@
+public class LogicalOperator {
+    public static void main(String[] args) {
+        System.out.println("Logical Operators");
+        int a = 5;
+        int b= 4;
+
+        System.out.println( (a > b) && (5<4));
+        System.out.println((a ==b) || (3>5));
+        System.out.println(!(a==b));
+
+    }
+}
