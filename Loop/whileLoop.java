a4d900e94cf3f706f037b085f2f8be5122f2f57f
Nishan Kumar Rai <infonkumarrai323@gmail.com>
2025-05-18 21:09:13 +0545

    while loop

---
+++ Loop/whileLoop.java
@@ -0,0 +1,11 @@
+package Loop;
+
+public class whileLoop {
+    public static void main(String[] args) {
+        int  counter =0;
+        while(counter<=100){
+            System.out.println("Hello world" + counter);
+            counter ++;
+        }
+    }
+}
